ad4fe6325d4ba6be63531dc1d13473582c56cd71
Mike Wilson <michael.wilson@port.ac.uk>
2012-04-19 11:13:15 +0100

    first commit

---
+++ lang/en/recordingsbn.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Language File - English (en)
+ *
+ * Authors:
+ *    Jesus Federico (jesus [at] b l i n ds i de n  e t w o r ks [dt] com)
+ *
+ * @package   mod_bigbluebutton
+ * @copyright 2011-2012 Blindside Networks Inc.
+ * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v2 or later
+ */
+defined('MOODLE_INTERNAL') || die();
+
+$string['modulename'] = 'RecordingsBN';
+$string['modulenameplural'] = 'RecordingsBN';
+$string['modulename_help'] = 'Use the recordingsbn module as a resource of the course in order to have access to the playback recordings related to it.';
+$string['recordingsbnname'] = 'Recordings name';
+$string['recordingsbnname_help'] = 'RecordingsBN provides a list of playback recordings in a BigBlueButton Server providing direct access to them.';
+$string['recordingsbn'] = 'RecordingsBN';
+$string['pluginadministration'] = 'recordingsbn administration';
+$string['pluginname'] = 'RecordingsBN';
+$string['recordingsbn:view'] = 'View recordings';
+
+$string['view_noguests'] = 'The RecordingsBN module is not open to guests';
+?>
